77122dc7db004f8a6b83ba0bd089ffb9e0ea3429
Takuya Matsuyama <hi@craftz.dog>
2023-03-30 14:55:38 +0900

    feat(api): run download-audio.sh

--- pages/api/audio.ts
+++ pages/api/audio.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { spawn } from 'child_process'
+import path from 'path'
 
 export default function GET(
   request: NextApiRequest,
@@ -11,4 +13,27 @@ export default function GET(
   }
 
   console.log('video ID:', video_id)
+  const cmd = spawn(path.join(process.cwd(), 'scripts/download-audio.sh'), [
+    video_id || ''
+  ])
+  cmd.on('close', code => {
+    console.log('Finished command. Exit code:', code)
+  })
+  cmd.stderr.on('data', chunk => {
+    const chunkStr = chunk.toString('utf-8')
+    console.error('[Error]', chunkStr)
+    response.write(
+      chunkStr
+        .split('\n')
+        .map((line: string) => '[Error] ' + line)
+        .join('\n')
+    )
+  })
+
+  response.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Cache-Control': 'no-cache',
+    'Content-Encoding': 'none'
+  })
+  cmd.stdout.pipe(response)
 }
