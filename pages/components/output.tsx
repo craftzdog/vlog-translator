900e39966cc598e95a43f05f879cfbd02eac44ae
Takuya Matsuyama <hi@craftz.dog>
2023-03-30 14:33:15 +0900

    feat(ui): automatically scroll down in the output box

--- pages/components/output.tsx
+++ pages/components/output.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { styled } from '@stitches/react'
 import { Box } from './box'
 
@@ -7,8 +7,20 @@ type Props = {
 }
 
 export const Output: React.FC<Props> = ({ children }) => {
+  const refBox = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const elBox = refBox.current
+    if (elBox) {
+      elBox.scrollTop = elBox.scrollHeight
+    }
+  }, [children])
   return (
-    <Box css={{ flex: 1, overflow: 'auto' }} className="output-container">
+    <Box
+      ref={refBox}
+      css={{ flex: 1, overflow: 'auto' }}
+      className="output-container"
+    >
       <Pre>{children}</Pre>
     </Box>
   )
