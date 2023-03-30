1e03662be2c74727dbefaf3901b76b525a85504e
Takuya Matsuyama <hi@craftz.dog>
2023-03-30 11:27:01 +0900

    feat(script): add a prompt to translate text into Japanese

--- scripts/translate.py
+++ scripts/translate.py
@@ -1,10 +1,13 @@
 import os
 import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
-result = openai.Completion.create(
+
+prompt_base = "You are going to be a good translator. Here is a part of the transcript of my vlog. I am talking about my product called Inkdrop, which is a Markdown note-taking app designed for developers. Translate the following text precisely into Japanese with the polite and formal style. Translate from [START] to [END]:\n[START]"
+
+response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Say this is a test",
-  max_tokens=7,
+  prompt=prompt_base+"Say this is a test" + "[END]",
+  max_tokens=70,
   temperature=0
 )
-print(result)
+print(response.choices[0].text)
