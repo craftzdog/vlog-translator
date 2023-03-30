e3f21982ff297f3e32e661e71bcd8e789441d923
Takuya Matsuyama <hi@craftz.dog>
2023-03-30 11:48:43 +0900

    feat(script): translate each subtitle

--- scripts/translate.py
+++ scripts/translate.py
@@ -6,15 +6,21 @@ import pysrt
 openai.api_key = os.getenv("OPENAI_API_KEY")
 input_data = sys.stdin.read()
 subs = pysrt.from_string(input_data)
-for index, subtitle in enumerate(subs):
-    print(index, subtitle.text)
 
-# prompt_base = "You are going to be a good translator. Here is a part of the transcript of my vlog. I am talking about my product called Inkdrop, which is a Markdown note-taking app designed for developers. Translate the following text precisely into Japanese with the polite and formal style. Translate from [START] to [END]:\n[START]"
-#
-# response = openai.Completion.create(
-#   model="text-davinci-003",
-#   prompt=prompt_base+"Say this is a test" + "[END]",
-#   max_tokens=70,
-#   temperature=0
-# )
-# print(response.choices[0].text)
+prompt_base = "You are going to be a good translator. Here is a part of the transcript of my vlog. I am talking about my product called Inkdrop, which is a Markdown note-taking app designed for developers. Translate the following text precisely into Japanese with the polite and formal style. Translate from [START] to [END]:\n[START]\n"
+
+def translate_text(text):
+    prompt = prompt_base
+    prompt += text + "\n[END]"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=3000,
+        temperature=0,
+    )
+    return response.choices[0].text.strip()
+
+for index, subtitle in enumerate(subs):
+    subtitle.text = translate_text(subtitle.text)
+    print(subtitle)
