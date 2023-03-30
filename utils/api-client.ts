19c8d747aad289b228c08e195f97d8d10f9cc0de
Takuya Matsuyama <hi@craftz.dog>
2023-03-30 15:33:29 +0900

    feat(api): download audio

--- utils/api-client.ts
+++ utils/api-client.ts
@@ -9,11 +9,50 @@ export async function processVideo(
   callback: ProgressCallback
 ): Promise<false | string> {
   callback('Downloading audio...\n')
-  callback('Progress 1\n')
-  callback('Progress 2\n')
-  callback('Progress 3\n')
-  callback('Progress 4\n')
-  callback('Progress 5\n')
+  await downloadAudio(videoId, callback)
 
   return 'result'
+}
+
+export async function downloadAudio(
+  videoId: string,
+  onProgress: ProgressCallback
+) {
+  const res = await fetch(
+    `/api/audio?${new URLSearchParams({ video_id: videoId })}`,
+    {}
+  )
+  const reader = res.body?.getReader()
+
+  if (reader) {
+    return streamResponse(reader, onProgress)
+  } else {
+    return false
+  }
+}
+
+async function streamResponse(
+  reader: ReadableStreamDefaultReader<Uint8Array>,
+  onProgress: ProgressCallback
+): Promise<string> {
+  return await new Promise(resolve => {
+    const decoder = new TextDecoder()
+    let result = ''
+    const readChunk = ({
+      done,
+      value
+    }: ReadableStreamReadResult<Uint8Array>) => {
+      if (done) {
+        resolve(result)
+        return
+      }
+
+      const output = decoder.decode(value)
+      result += output
+      onProgress(output)
+      reader.read().then(readChunk)
+    }
+
+    reader.read().then(readChunk)
+  })
 }
