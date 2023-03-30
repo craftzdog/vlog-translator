5a34e538b20c3d4c04e65194e520252829ee52e2
Takuya Matsuyama <hi@craftz.dog>
2023-03-30 11:17:31 +0900

    feat(script): call whisper API

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -3,6 +3,13 @@ import openai
 import sys
 openai.api_key = os.getenv("OPENAI_API_KEY")
 video_id = sys.argv[1]
-audio_file = os.path.join(os.getcwd(), 'tmp', video_id + '.m4a')
+audio_file_path = os.path.join(os.getcwd(), 'tmp', video_id + '.m4a')
 
-print(audio_file)
+audio_file = open(audio_file_path, "rb")
+transcript = openai.Audio.transcribe(
+  file = audio_file,
+  model = "whisper-1",
+  response_format='srt',
+  prompt = 'I am a programmer. This is a vlog about my app development, tech review, lifehacks, etc. I have my own product called Inkdrop.'
+)
+print(transcript)
