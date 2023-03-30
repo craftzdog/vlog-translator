13c0aa23c63918b3ed541c7a07cdde8d8e035e7e
Takuya Matsuyama <hi@craftz.dog>
2023-03-30 14:54:26 +0900

    feat(api): get url query

--- pages/api/audio.ts
+++ pages/api/audio.ts
@@ -1,5 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'John Doe' })
+export default function GET(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const video_id = request.query.video_id as string
+  if (typeof video_id !== 'string') {
+    response.status(400).json({ error: 'Invalid request' })
+    return
+  }
+
+  console.log('video ID:', video_id)
 }
