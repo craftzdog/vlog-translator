6bcf76df60e4452d0547cb0407e863378986bf95
Takuya Matsuyama <hi@craftz.dog>
2023-03-30 11:34:09 +0900

    feat(script): get input from stdin

--- scripts/translate.py
+++ scripts/translate.py
@@ -1,13 +1,17 @@
 import os
+import sys
 import openai
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
+input_data = sys.stdin.read()
 
-prompt_base = "You are going to be a good translator. Here is a part of the transcript of my vlog. I am talking about my product called Inkdrop, which is a Markdown note-taking app designed for developers. Translate the following text precisely into Japanese with the polite and formal style. Translate from [START] to [END]:\n[START]"
-
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=prompt_base+"Say this is a test" + "[END]",
-  max_tokens=70,
-  temperature=0
-)
-print(response.choices[0].text)
+print(input_data)
+# prompt_base = "You are going to be a good translator. Here is a part of the transcript of my vlog. I am talking about my product called Inkdrop, which is a Markdown note-taking app designed for developers. Translate the following text precisely into Japanese with the polite and formal style. Translate from [START] to [END]:\n[START]"
+#
+# response = openai.Completion.create(
+#   model="text-davinci-003",
+#   prompt=prompt_base+"Say this is a test" + "[END]",
+#   max_tokens=70,
+#   temperature=0
+# )
+# print(response.choices[0].text)
