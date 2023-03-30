f8791865c48ea0c17807ff1c0207413e9a3018c6
Takuya Matsuyama <hi@craftz.dog>
2023-03-30 11:07:47 +0900

    fix(scripts): get a path to the audio file

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -3,5 +3,6 @@ import openai
 import sys
 openai.api_key = os.getenv("OPENAI_API_KEY")
 video_id = sys.argv[1]
+audio_file = os.path.join(os.getcwd(), 'tmp', video_id + '.m4a')
 
-print(video_id)
+print(audio_file)
