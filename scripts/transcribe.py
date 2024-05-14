b8646364f4de9082117f81d9e88d0d348a38b594
Takuya Matsuyama <hi@craftz.dog>
2024-05-14 11:27:13 +0900

    fix(scripts): do not automatically add a file extension

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -2,8 +2,8 @@ import os
 import openai
 import sys
 openai.api_key = os.getenv('OPENAI_API_KEY')
-video_id = sys.argv[1]
-audio_file_path = os.path.join(os.getcwd(), 'tmp', video_id + '.m4a')
+audio_filename = sys.argv[1]
+audio_file_path = os.path.join(os.getcwd(), 'tmp', audio_filename)
 
 prompt_file_path = os.path.join(os.getcwd(), 'tmp', 'prompt.txt')
 f = open(prompt_file_path, "r")
